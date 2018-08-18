f253343d3391151d619a5eebc979d21faaa46cf8
Unknown <przemek.rakowski@gmail.com>
2018-08-18 13:14:01 +0200

    Komentarze

--- src/Galaxy.js
+++ src/Galaxy.js
@@ -1,13 +1,13 @@
 
 class Star {
   constructor() {
-    var newStar = document.createElement("div");
-    newStar.textContent = 'Czesc';
+    var newStar = document.createElement('div');
+    newStar.textContent = 'Czesc'; // https://stackoverflow.com/questions/35213147/difference-between-text-content-vs-inner-text link do stacaka, zobacz sobie tez na innerText i co za sobą niosą oba rozwiązania /PR
 
-    var star = document.querySelector('star');
-    document.body.insertBefore(newStar,star);
+    var star = document.querySelector('star'); // brakuje # przed star i temu nie działą /PR
+    document.body.insertBefore(newStar, star); // zobacz raczej na appendChild /PR
 
-    console.log ("Dziala")
+    console.log('Dziala');
   }
 }
 
--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,7 @@
 import Star from './Galaxy';
+//nazywajcie importy w taki sposób zeby myliło bo Star z Galaxy trochę miesza / PR
+//a w Pliku index.html nie tworzymy diva na gwiazdę, powinny być generowane w js
+
 
 class Game {
   constructor() {
@@ -8,9 +11,10 @@ class Game {
     //tutaj bedzie sie tworzyl obiekt Star
     this.star = new Star();
   }
-}
+}// tutaj jest klamra zamykająca klase /PR
 
-start = () => {
+// wszystkie metody powinny być w środku klasy, inaczej nie będzie działać /PR
+start = () => { 
   console.log('Game started');
 }
 
