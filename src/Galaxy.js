434dacd774e3d3bab3f7c5b9ac4bcc1cfccfd591
Oleksandra <oleksandra.chernyak@gmail.com>
2018-08-19 07:43:45 +0200

    poprawki

--- src/Galaxy.js
+++ src/Galaxy.js
@@ -3,9 +3,6 @@ class Galaxy {
   constructor () {
     this.star = document.createElement('div');
     this.currentStarIndex = 0;
-    this.color = color;
-    this.width = width;
-    this.height = height;
   }
 
 }
