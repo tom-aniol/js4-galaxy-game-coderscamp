a9f35b34f75505a9796b0e5947bad2b6a9954fa2
Unknown <vic.sobolak@gmail.com>
2018-08-20 18:57:23 +0200

    star hover function

--- src/Galaxy.js
+++ src/Galaxy.js
@@ -7,15 +7,45 @@ class Galaxy {
   constructor(starCount) {
     this.starCount = starCount
     this.gameArea = document.querySelector('#gamearea')
+    this.order = 1
   }
+
+
+  hover(star) {
+    if (this.order >= this.starCount) {
+      this.gameArea.innerHTML = ''
+      this.order = 1
+      setTimeout(()=> {
+        this.createSpace();
+      }, 300)
+      return
+
+    }
+
+    if (this.order == star.innerText) {
+      star.remove()
+      this.order++
+    } else {
+      this.gameArea.innerHTML = ''
+      setTimeout(() => {
+        this.createSpace();
+      }, 300)
+      this.order = 1
+    }
+
+
+  }
+
 
   createSpace() {
     for (let i = 0; i < this.starCount; i++) {
-      let star = new Star(i)
-
-      star.create()
+      let newStar = new Star(i)
+      newStar.star.addEventListener('mouseover', (e) => this.hover(e.target))
+      newStar.create()
     }
   }
+
+
 }
 
 export default Galaxy;
