709d6b3e5b12af5cf05ef08124ddb485c3b879a6
Tomasz Anioł <156411@student.ue.wroc.pl>
2018-08-20 18:25:00 +0200

    Tomek - generowanie gwiazd

--- src/Galaxy.js
+++ src/Galaxy.js
@@ -1,10 +1,20 @@
 //
 
+import Star from './Star'
 
 
 class Galaxy {
-  constructor() {
+  constructor(starCount) {
+    this.starCount = starCount
+    this.gameArea = document.querySelector('#gamearea')
+  }
 
+  createSpace() {
+    for (let i = 0; i < this.starCount; i++) {
+      let star = new Star(i)
+
+      star.create()
+    }
   }
 }
 
--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,16 @@
 //
 
-import Star from './Star';
+import Galaxy from './Galaxy';
 
 class Game {
   constructor() {
-
+    this.galaxy = new Galaxy(5)
   }
 
+  start() {
+    this.galaxy.createSpace()
+    console.log(this.galaxy)
+  }
 }
 
 export default Game;
