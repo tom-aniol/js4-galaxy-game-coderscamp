52c7f121ad1b7f2ed6eb35d67c3f4695ed611bc8
Unknown <vic.sobolak@gmail.com>
2018-08-20 18:57:23 +0200

    star hover function

--- src/Galaxy.js
+++ src/Galaxy.js
@@ -1,21 +1,57 @@
 //
 
 import Star from './Star'
+import Score from './Score'
 
 
 class Galaxy {
   constructor(starCount) {
     this.starCount = starCount
     this.gameArea = document.querySelector('#gamearea')
+    this.order = 1
+    this.score = new Score()
   }
+
+
+  hover(star) {
+    if (this.order >= this.starCount) {
+      this.gameArea.innerHTML = ''
+      this.order = 1
+      this.score.addPoints()
+
+      setTimeout(()=> {
+        this.createSpace();
+      }, 300)
+      return
+
+    }
+
+    if (this.order == star.innerText) {
+      star.remove()
+      this.order++
+      this.score.addPoints()
+    } else {
+      this.gameArea.innerHTML = ''
+      this.score.resetPoints()
+      setTimeout(() => {
+        this.createSpace();
+      }, 300)
+      this.order = 1
+    }
+
+
+  }
+
 
   createSpace() {
     for (let i = 0; i < this.starCount; i++) {
-      let star = new Star(i)
-
-      star.create()
+      let newStar = new Star(i)
+      newStar.star.addEventListener('mouseover', (e) => this.hover(e.target))
+      newStar.create()
     }
   }
+
+
 }
 
 export default Galaxy;
--- src/Score.js
+++ src/Score.js
@@ -1,7 +1,24 @@
 //
 
 class Score {
-  constructor() {}
+  constructor() {
+    this.points = 0
+    this.score = document.querySelector('#scorenumber')
+
+    this.score.innerText = this.points
+
+      }
+
+      addPoints(){
+        this.points++
+        this.score.innerText = this.points
+
+      }
+
+  resetPoints() {
+    this.points = 0
+    this.score.innerText = this.points
+  }
 }
 
 export default Score;
